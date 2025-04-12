190a8e249ee120ce13ffd65a08a2ac5a3af47b79
Evgeniya <eskosmacheva@edu.hse.ru>
2025-04-12 22:53:29 +0500

    first commit

--- Lesson 1.py
+++ Lesson 1.py
@@ -4,7 +4,8 @@ print(name + ' ' +surname)
 company_list = ["Like", "Smile"]
 
 
-# first task with different types
+# first task with different types Поработайте с переменными, создайте несколько, выведите на экран.
+# Запросите у пользователя некоторые числа и строки и сохраните в переменные, а затем выведите на экран.
 print (company_list)
 int_number_1 = 52
 print(int_number_1)
@@ -17,7 +18,8 @@ print(List_example_1)
 dict_example_1={"Name": "Steve","Surname": "Jobes", 'age': '21'}
 
 
-# Task with inputs
+# Task with inputs Используйте функции для консольного ввода input() и консольного вывода print().
+# Попробуйте также через встроенную функцию id() понаблюдать, какие типы объектов могут изменяться и сохранять за собой адрес в оперативной памяти.
 phrase = input('Write your name: ')
 print(phrase)
 phrase_2 = input ("Write your age:")
@@ -30,7 +32,8 @@ print (phrase_3)
 
 
 
-# task with time
+# task with time Предусмотрите проверку строки на наличие только числовых данных через встроенный строковый метод .isdigit().
+# Выведите рассчитанные часы, минуты и секунды по отдельности в консоль.
 if phrase_3.isdigit():
     total_seconds=int(phrase_3)
     hours=total_seconds//3600
@@ -49,7 +52,7 @@ else:
 
 
 
-    # task with numbers
+    # task with numbers Запросите у пользователя через консоль число n (от 1 до 9). Найдите сумму чисел n + nn + nnn.
 n_input = input('Write number from 1 to 9:')
 if n_input.isdigit() and 1 <= int(n_input) <= 9:
     n = int(n_input)
