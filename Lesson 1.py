bb6bba66d51af405f8e335fde1819803599ee2ee
Evgeniya <eskosmacheva@edu.hse.ru>
2025-04-12 16:50:33 +0500

    first commit

--- Lesson 1.py
+++ Lesson 1.py
@@ -3,6 +3,7 @@ surname = 'Education'
 print(name + ' ' +surname)
 
 company_list = ["Like", "Smile"]
+# first task with different types
 print (company_list)
 int_number_1 = 52
 print(int_number_1)
@@ -13,14 +14,17 @@ print (str_first)
 List_example_1=['1,8,81,102,17']
 print(List_example_1)
 dict_example_1={"Name": "Steve","Surname": "Jobes", 'age': '21'}
+# Task with inputs
 phrase = input('Write your name: ')
 print(phrase)
 phrase_2 = input ("Write your age:")
 print (phrase_2)
+# task with id
 object_id=id(float_number_2)
 print (object_id)
 phrase_3=input ('Wtite time in seconds:')
 print (phrase_3)
+# task with time
 if phrase_3.isdigit():
     total_seconds=int(phrase_3)
     hours=total_seconds//3600
@@ -36,6 +40,7 @@ if phrase_3.isdigit():
     print("Секунды:", seconds_var)
 else:
     print("Ошибка: введите целое число секунд.")
+    # task witn numbers
 n_input = input('Write number from 1 to 9:')
 if n_input.isdigit() and 1 <= int(n_input) <= 9:
     n = int(n_input)
