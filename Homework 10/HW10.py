5bf20e9e54706db7f12718fa2a9122c089869931
Evgeniya <eskosmacheva@edu.hse.ru>
2025-09-18 20:41:56 +0500

    HW 8

--- Homework 10/HW10.py
+++ Homework 10/HW10.py
@@ -1,122 +1,58 @@
-#task 1
-import random
-from array import * # импортирует все функции из модуля array
-a=random.randint(3,5)
-array=array('i') # i-целые числа, f- float,d - double (вещественное число с двойной точностью), u-строки, символы
-for i in range (10,250000000,a):
-    array.append(i)
+# dz_10_sorting_tasks.py
+from random import randint, seed
+from time import perf_counter
 
-while True:
-    try:
-        number=int (input ("Введите число:"))
-        if number in array:
+seed(42)
+
+N = 100_000
+
+arr1 = [randint(1, 1_000_000) for _ in range(N)]
+arr2 = [
+    {"num_1": randint(1, 1_000_000), "num_2": randint(1, 1_000_000)}
+    for _ in range(N)
+]
+
+
+def bubble_sort(a: list) -> list:
+    n = len(a)
+    while True:
+        swapped = False
+        last_swap = 0
+        for i in range(1, n):
+            if a[i - 1] > a[i]:
+                a[i - 1], a[i] = a[i], a[i - 1]
+                swapped = True
+                last_swap = i
+        n = last_swap
+        if not swapped:
             break
-        else:
-            print("Такого числа нет в массиве")
-    except ValueError:
-        print("Введите другое число")
+    return a
 
-find_index=-1
-start_array = 0
-finish_array=len(array)-1
-while find_index==-1:
-    central_int = (start_array+finish_array) // 2
-    if number>array[central_int]:
-        start_array=central_int
-    elif number<array [central_int]:
-        finish_array=central_int
+
+def main():
+    print(f"Generated: arr1={len(arr1):,} ints, arr2={len(arr2):,} dicts")
+
+    FORCE_FULL_BUBBLE_SORT = False
+    BUBBLE_DEMO_SIZE = 5_000
+
+    if FORCE_FULL_BUBBLE_SORT:
+        data_for_bubble = arr1.copy()
+        label = "bubble 100k"
     else:
-        find_index=central_int
-print(find_index)
+        data_for_bubble = arr1[:BUBBLE_DEMO_SIZE]
+        label = f"bubble demo {BUBBLE_DEMO_SIZE}"
+
+    t0 = perf_counter()
+    bubble_sort(data_for_bubble)
+    t1 = perf_counter()
+    print(f"{label}: {t1 - t0:.3f}s | sorted? {data_for_bubble == sorted(data_for_bubble)}")
 
 
-#task 2
-
-numbers=[random.randint(0,2000) for i in range (10)]
-print (numbers)
-
-# task 3
-def line_search (a,results):
-    find_index=-1
-    i=0
-    while find_index==-1:
-        if a==results[i]:
-           return i
-        else:
-            i+=1
-list_of_results=[i for i in range(0,100)]
-print (line_search (int(input( "Введите число:")),list_of_results))
-
-# task 4
-array_4=[i for i in range(0,100)]
-while True:
-    try:
-        number=int (input ("Введите число:"))
-        if number in array_4:
-            break
-        else:
-            print("Такого числа нет в массиве")
-    except ValueError:
-        print("Введите другое число")
-def binar_search(number,array_4):
-    find_index=-1
-    start_array = 0
-    finish_array=len(array_4)-1
-    while find_index==-1:
-        central_int = (start_array+finish_array) // 2
-        if number>array_4[central_int]:
-            start_array=central_int
-        elif number<array_4 [central_int]:
-            finish_array=central_int
-        else:
-            find_index=central_int
-    print(find_index)
-binar_search (number, array_4)
+    t2 = perf_counter()
+    arr2.sort(key=lambda d: (d["num_1"], d["num_2"]))
+    t3 = perf_counter()
+    print(f"arr2 .sort(key=(num_1,num_2)): {t3 - t2:.3f}s")
 
 
-
-# task 5
-import time
-a=random.randint(3,5)
-array=array('i') # i-целые числа, f- float,d - double (вещественное число с двойной точностью), u-строки, символы
-for i in range (10,5000):
-    array.append(i)
-while True:
-    number=random.randint(10,5000)
-    if number in array:
-        break
-
-
-def line_search (a,results):
-    find_index=-1
-    i=0
-    while find_index==-1:
-        if a==results[i]:
-           print(i)
-           break
-        else:
-            i+=1
-
-def binar_search(number,array_4):
-    find_index=-1
-    start_array = 0
-    finish_array=len(array_4)-1
-    while find_index==-1:
-        central_int = (start_array+finish_array) // 2
-        if number>array_4[central_int]:
-            start_array=central_int
-        elif number<array_4 [central_int]:
-            finish_array=central_int
-        else:
-            find_index=central_int
-    print(find_index)
-
-start_time=time.time ()
-line_search(number,array)
-end_time=time.time()
-print (f"Время выполнения поиска: {end_time-start_time}")
-
-start_time=time.time ()
-binar_search (number,array)
-end_time=time.time()
-print (f"Время выполнения поиска: {end_time-start_time}")
+if __name__ == "__main__":
+    main()
